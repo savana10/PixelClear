fda2739215aefab1277783fadd896cc5afcc3abf
savana kranth <savanakranth@gmail.com>
2020-08-09 13:31:03 +0545

    Initial Commit

---
+++ PixelClear/PixelClear.h
@@ -0,0 +1,19 @@
+//
+//  PixelClear.h
+//  PixelClear
+//
+//  Created by savana kranth on 09/08/2020.
+//  Copyright © 2020 savana kranth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PixelClear.
+FOUNDATION_EXPORT double PixelClearVersionNumber;
+
+//! Project version string for PixelClear.
+FOUNDATION_EXPORT const unsigned char PixelClearVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PixelClear/PublicHeader.h>
+
+
